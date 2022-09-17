07a3794ad6abc71f0b7f83a1de0ad88ab185d6c1
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-09-17 19:45:25 +0800

    🤯 Back to flask

--- app.py
+++ app.py
@@ -1,55 +1,57 @@
 import json
 
 import requests
-from aiohttp import web
+from flask import Flask, request, jsonify
+from urllib.parse import parse_qs
 
 from config import *
 from database import SMMWEDatabase
 from smmwe_lib import *
 
-
-async def readme_handler(request):
-    return web.Response(
-        text=open('website/index.html', 'r').read().replace("${{Markdown}}", open('README.md', 'r').read()),
-        content_type='text/html')
+app = Flask(__name__)
 
 
-async def user_login_handler(request):
-    data = await request.args.to_dict()
-    login_user = {'username': data['alias'], 'admin': False, 'mod': False, 'booster': False, 'goomba': True,
-                  'alias': data['alias'],
-                  'id': '0000000000', 'uploads': '0',
-                  'auth_code': data['alias']}
+@app.route('/', methods=['GET'])
+async def readme_handler():
+    return open('website/index.html', 'r').read().replace("${{Markdown}}", open('README.md', 'r').read())
+
+
+@app.route('/user/login', methods=['POST'])
+async def user_login_handler():
+    data = parse_qs(request.get_data().decode('utf-8'))
+    login_user = {'username': data['alias'][0], 'admin': False, 'mod': False, 'booster': False, 'goomba': True,
+                  'alias': data['alias'][0],
+                  'id': '0000000000', 'uploads': '0'}
     if data['token'][0] == Tokens.PC_323:
         login_user['mobile'] = False
+        login_user['auth_code'] = data['alias'][0] + '|PC'
     else:
         login_user['mobile'] = True
-    return web.json_response(login_user)
+        login_user['auth_code'] = data['alias'][0] + '|Mobile'
+    return jsonify(login_user)
 
 
-async def stages_detailed_search_handler(request):
-    return web.Response(text='qwq')
+@app.route('/stages/detailed_search', methods=['POST'])
+async def stages_detailed_search_handler():
+    return 'qwq'
 
 
-async def stages_upload_handler(request):
-    data = request.args.to_dict()
+@app.route('/stage/upload', methods=['POST'])
+async def stages_upload_handler():
+    data = parse_qs(request.get_data().decode('utf-8'))
     print("Uploading level to storage backend...")
-    data_swe = data['swe']
+    data_swe = data['swe'][0]
     level_id = gen_level_id(data_swe)
     if len(data_swe.encode()) > 4 * 1024 * 1024:  # 4MB limit
         return json.dumps({'error_type': '028', 'message': 'File is larger than 4MB.'})
-    requests.post(url=STORAGE_URL, params={'upload': data['name'] + '.swe', 'key': STORAGE_AUTH_KEY},
+    requests.post(url=STORAGE_URL, params={'upload': data['name'][0] + '.swe', 'key': STORAGE_AUTH_KEY},
                   data=data_swe)  # Upload to storage backend
-    db.add_level(data['name'], data['aparience'], data['entorno'], data['tags'], data['auth_code'], level_id)
-    return web.json_response({'message': 'Upload completed.', 'error_type': level_id})
+    db.add_level(data['name'][0], data['aparience'][0], data['entorno'][0], data['tags'][0], data['auth_code'][0],
+                 level_id)
+    return jsonify({'message': 'Upload completed.', 'error_type': level_id})
 
 
 if __name__ == '__main__':
     db = SMMWEDatabase()
     db.Level.create_table()  # auto create table
-    app = web.Application()
-    app.add_routes([web.get('/', readme_handler),
-                    web.post('/user/login', user_login_handler),
-                    web.post('/stages/detailed_search', stages_detailed_search_handler),
-                    web.post('/stage/upload', stages_upload_handler)])
-    web.run_app(app, host=HOST, port=PORT)
+    app.run(host=HOST, port=PORT, debug=True)
