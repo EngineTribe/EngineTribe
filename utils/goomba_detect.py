89fbe52aa91d524a028be9a7693c14523d653021
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-10-13 14:54:54 +0800

    ⛔ User disabling system like original "Goomba"

---
+++ utils/goomba_detect.py
@@ -0,0 +1,33 @@
+from goomba_detect_config import *
+import sys
+
+sys.path.append('..')
+
+import discord, asyncio
+from database import SMMWEDatabase
+
+db = SMMWEDatabase()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}, disabling accounts ...')
+    guild = client.get_guild(GUILD_ID)
+    print(guild.name)
+    for user in db.User.select():
+        if len(user.user_id) > 10:  # Discord user
+            if guild.get_member(int(user.user_id)) not in guild.get_role(VERIFIED_ROLE).members:
+                print(f'Disable {user.username}\'s account')
+                user.is_valid = False
+                user.save()
+    asyncio.get_event_loop().stop()
+
+client.run(BOT_TOKEN)
