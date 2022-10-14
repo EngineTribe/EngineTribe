84eef381b3d7cf70fa7e9c8a58a2502eace1cf13
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-10-14 21:06:39 +0800

    🔧 Fix account disabling

--- utils/disable_accounts.py
+++ utils/disable_accounts.py
@@ -22,12 +22,19 @@ async def on_ready():
     print(f'We have logged in as {client.user}, disabling accounts ...')
     guild = client.get_guild(GUILD_ID)
     print(guild.name)
+    print(len(guild.get_role(VERIFIED_ROLE).members))
     for user in db.User.select():
-        if len(user.user_id) > 10 and user.is_valid:  # Discord user
-            if guild.get_member(int(user.user_id)) not in guild.get_role(VERIFIED_ROLE).members:
-                print(f'Disable {user.username}\'s account')
-                user.is_valid = False
-                user.save()
+        if len(user.user_id) > 10:  # Discord user
+            #print(guild.get_member(int(user.user_id)) not in guild.get_role(VERIFIED_ROLE).members)
+            member = guild.get_member(int(user.user_id))
+            if member:
+                if member not in guild.get_role(VERIFIED_ROLE).members:
+                    print(f'Disable {user.username} ({member.name})\'s account')
+                    user.is_valid = False
+                    #user.save()
+                else:
+                    user.is_valid = True
+                    #user.save()
     asyncio.get_event_loop().stop()
 
 client.run(BOT_TOKEN)
