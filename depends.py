bbf7079feb1d0920d37d6409733943734d9b5d34
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-10-28 17:46:30 +0800

    🎮 Added PlayStation Vita User-Agent

--- depends.py
+++ depends.py
@@ -9,21 +9,22 @@ connection_count = ContextVar("connection_count", default=0)
 
 
 def connection_count_inc():
-	connection_count.set(connection_count.get() + 1)
+    connection_count.set(connection_count.get() + 1)
 
 
 def is_valid_user(user_agent: Union[str, None] = Header(default=None)):
-	is_vaild_ua = False
-	if user_agent is not None:
-		is_vaild_ua = True
-	if (
-			("GameMaker" in user_agent)
-			or ("Android" in user_agent)
-			or ("EngineBot" in user_agent)
-	):
-		is_vaild_ua = True
-	if not is_vaild_ua:
-		raise HTTPException(
-			status_code=200,
-			detail=ErrorMessage(error_type="005", message="Illegal client."),
-		)
+    is_vaild_ua = False
+    if user_agent is not None:
+        is_vaild_ua = True
+    if (
+            ("GameMaker" in user_agent)  # Windows / Linux
+            or ("Android" in user_agent)  # Android
+            or ("EngineBot" in user_agent)  # Engine Bot
+            or ("PlayStation" in user_agent)  # PlayStation Vita
+    ):
+        is_vaild_ua = True
+    if not is_vaild_ua:
+        raise HTTPException(
+            status_code=200,
+            detail=ErrorMessage(error_type="005", message="Illegal client."),
+        )
