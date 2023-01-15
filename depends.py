05247371f95c5330963e2d9c9fc3f59751310117
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-01-15 12:42:47 +0800

    🔧 fix: do not verify user agent

--- depends.py
+++ depends.py
@@ -11,15 +11,15 @@ def connection_count_inc():
 
 
 def is_valid_user(user_agent: str | None = Header(default=None)):
-    if user_agent is None or not (
-            ("GameMaker" in user_agent)  # Windows / Linux
-            or ("Dalvik" in user_agent)  # Android
-            or ("Android" in user_agent)  # Android
-            or ("EngineBot" in user_agent)  # Engine Bot
-            or ("PlayStation" in user_agent)  # PlayStation Vita
-            or ("libcurl-agent" in user_agent)  # GMS2 (3.3.0+) Windows / Linux
-    ):
-        # if user_agent is None:
+    # if user_agent is None or not (
+    #         ("GameMaker" in user_agent)  # Windows / Linux
+    #         or ("Dalvik" in user_agent)  # Android
+    #         or ("Android" in user_agent)  # Android
+    #         or ("EngineBot" in user_agent)  # Engine Bot
+    #         or ("PlayStation" in user_agent)  # PlayStation Vita
+    #         or ("libcurl-agent" in user_agent)  # GMS2 (3.3.0+) Windows / Linux
+    # ):
+    if user_agent is None:
         raise ErrorMessageException(
             error_type="005",
             message="Illegal client.")
