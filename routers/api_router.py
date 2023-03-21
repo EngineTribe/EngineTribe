3fc8fae4bc0fb4e68dcd9627c664cc3e9bee5f1e
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2023-03-21 22:20:38 +0800

    🔧 Fix 3.3.3 workarounds

--- routers/api_router.py
+++ routers/api_router.py
@@ -1,17 +1,27 @@
 from typing import Any, Callable
+
 from fastapi import APIRouter as FastAPIRouter
+from fastapi.types import DecoratedCallable
 
 
 class APIRouter(FastAPIRouter):
-    def add_api_route(
-            self, path: str, endpoint: Callable[..., Any], *,
-            include_in_schema: bool = False, **kwargs: Any
-    ) -> None:
+    def api_route(
+        self, path: str, *, include_in_schema: bool = False, **kwargs: Any
+    ) -> Callable[[DecoratedCallable], DecoratedCallable]:
         if path.endswith("/"):
-            alternate_path = path[:-1]
-        else:
-            alternate_path = path + "/"
-        super().add_api_route(
-            alternate_path, endpoint, include_in_schema=False, **kwargs)
-        return super().add_api_route(
-            path, endpoint, include_in_schema=include_in_schema, **kwargs)
+            path = path[:-1]
+
+        add_path = super().api_route(
+            path, include_in_schema=include_in_schema, **kwargs
+        )
+
+        alternate_path = path + "/"
+        add_alternate_path = super().api_route(
+            alternate_path, include_in_schema=False, **kwargs
+        )
+
+        def decorator(func: DecoratedCallable) -> DecoratedCallable:
+            add_alternate_path(func)
+            return add_path(func)
+
+        return decorator
